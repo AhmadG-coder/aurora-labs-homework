d232680e2ea328447adc1fafcc0a234cd7721809
aurora <aurora@aurora.com>
2022-10-08 22:47:30 +0300

    final commit
    Signed-off-by: aurora <aurora@aurora.com>

--- setup.sh
+++ setup.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 docker-compose up -d
+sleep 10 # until URL is available
 jenkins_url=$(python3 setup_webhooks.py)
 echo "JENKINS is now accessable at $jenkins_url"
+json_configuration=$(jq '.url = $jenkins_url' --arg jenkins_url $jenkins_url webhook_configuration.json)
+echo $json_configuration > webhook_configuration.json
+json_configuration=$(jq '.url += "/github-webhook/"' webhook_configuration.json)
+echo $json_configuration > webhook_configuration.json
 curl \
   -X PATCH \
     -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer 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" \
-    -d '{"url": "$jenkins_url","content_type":"json","insecure_ssl":"0"}'\
+    -H "Authorization: Bearer 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  " \
+    -d "$json_configuration" \
       https://api.github.com/repos/AhmadG-coder/aurora-labs-homework/hooks/383056256/config
 
 
